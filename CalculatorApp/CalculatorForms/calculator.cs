acb1829daab1719dd02ef59e30c591cbc13fb42d
theDarkCoderJerry <40047337+theDarkCoderJerry@users.noreply.github.com>
2019-01-24 13:18:14 +0530

    Added comments

--- CalculatorApp/CalculatorForms/calculator.cs
+++ CalculatorApp/CalculatorForms/calculator.cs
@@ -8,19 +8,22 @@ namespace CalculatorApp
 {
     public class Calculator
     {
-      
+      //Addition
         public float Add (float num1, float num2)
         {
         return num1 +num2;
         }
+        //substraction
         public float Sub (float num1, float num2)
         {
             return num1 - num2;
         }
+        //Multiplication
         public float Mul (float num1, float num2)
         {
             return num1 * num2;
         }
+        //Division
         public float Div (float num1, float num2)
         {
             return num1/num2;
